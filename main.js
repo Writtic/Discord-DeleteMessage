dc4b9f4dad768f9b1a0f276e40fdf90e04f071b6
Jun Seong, Park <writtic@gmail.com>
2018-03-04 01:55:50 +0900

    Erase unnecessary conditions

--- main.js
+++ main.js
@@ -2,11 +2,8 @@ var authToken = document.body.appendChild(document.createElement `iframe`).conte
 var username = '???'
 var all = true
 var beforeId = null
-if (typeof(blockedAuthors) === 'undefined') {
-  var blockedAuthors = []
-}
 
-clearMessages = function() {
+clearMessages = function () {
   const channel = window.location.href.split('/').pop()
   const baseURL = `https://discordapp.com/api/channels/${channel}/messages`
   const headers = {
@@ -15,7 +12,6 @@ clearMessages = function() {
 
   let clock = 0
   let interval = 1000
-  // let beforeId = null
   let messagesStore = []
 
   function delay(duration) {
@@ -25,35 +21,32 @@ clearMessages = function() {
   }
 
   function loadMessages() {
-
     let url = `${baseURL}?limit=100`
 
     if (beforeId) {
       url += `&before=${beforeId}`
     }
-    console.log(beforeId)
+    console.log(`beforeId: ${beforeId}`)
     return fetch(url, {
       headers
     })
   }
 
   function tryDeleteMessage(message) {
-    if (blockedAuthors.indexOf(message.author.id) === -1 && message.author.username === username) {
-
-      console.log(`Deleting message from ${message.author.username} (${message.content.substring(0, 30)}...)`)
+    if (message.author.username === username) {
+      console.log(`Deleting message from ${message.author.username} (${message.content.substring(0, 30)}...) at ${message.timestamp}`)
 
       return fetch(`${baseURL}/${message.id}`, {
         headers,
         method: 'DELETE'
       })
     } else {
-      console.log(`Not deleteing message from ${message.author.username} (${message.content.substring(0, 30)}...)`)
+      console.log(`Not deleteing message from ${message.author.username} (${message.content.substring(0, 30)}...) at ${message.timestamp}`)
     }
   }
 
   function filterMessages(message) {
-    if(all) {return (blockedAuthors.indexOf(message.author.id) === -1)}
-    else {return (blockedAuthors.indexOf(message.author.id) === -1 && message.author.username === username)}
+      return (message.author.username === username)
   }
 
   function onlyNotDeleted(message) {
@@ -76,42 +69,30 @@ clearMessages = function() {
 
       messagesStore = messagesStore.concat(messages.filter(filterMessages))
 
-      return Promise.all(messagesStore.filter(onlyNotDeleted).map(message => {
-        return delay(clock += interval)
-          .then(() => tryDeleteMessage(message))
-          .then(resp => {
-            if (resp && resp.status === 204) {
-              message.deleted = true
-              return resp.text()
-            }
-          })
-          .then(result => {
-            if (result) {
-              result = JSON.parse(result)
+      console.log(`messagesStore length: ${messagesStore.length}`)
 
-              if (result.code === 50003) {
-                console.log(`Cannot delete messages from user ${message.author.username}, skiping it`)
-
-                blockedAuthors.push(message.author.id)
-
-                messagesStore = messagesStore.filter(filterMessages)
+      if (messagesStore.length > 0) {
+        return Promise.all(messagesStore.filter(onlyNotDeleted).map(message => {
+          return delay(clock += interval)
+            .then(() => tryDeleteMessage(message))
+            .then(resp => {
+              if (resp && resp.status === 204) {
+                message.deleted = true
+                return resp.text()
               }
-            }
-          })
-      }))
-    })
-    .then(function() {
-      if (messagesStore.length !== 0 && messagesStore.length < 100) {
-        clearMessages()
+            })
+            .then(result => {
+              if (result)
+                result = JSON.parse(result)
+            })
+        }))
       } else {
-        console.log(`Finished clearing cycle. You can run again this script if you want delete next 100 messages`)
-        neverEndingStory();
+        return
       }
     })
+    .then(function () {
+      clearMessages()
+    })
 }
 
-function neverEndingStory() {
-  clearMessages()
-}
-
-neverEndingStory()
+clearMessages()
